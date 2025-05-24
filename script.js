630906acc94f715abea56aadc599cc804fc80d0b
rsivaku <117036297+rsivaku@users.noreply.github.com>
2025-05-24 15:32:54 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const githubButton = document.getElementById("github-button");
+const linkedInButton = document.getElementById("linkedin-button");
+
+githubButton.addEventListener("click", function() {
+    window.location.href = "https://github.com/";
+
+})
+
+linkedInButton.addEventListener("click", function() {
+    window.location.href = "https://www.linkedin.com/";
+
+})
